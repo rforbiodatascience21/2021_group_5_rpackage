fa7e5908c3a54aa3a754dfbd4f8b6a935beb7ec8
LucilleAugusta <s184250@student.dtu.dk>
2021-03-22 09:40:05 +0000

    Added comments to the code

--- R/dna_codons_to_aa.R
+++ R/dna_codons_to_aa.R
@@ -1,2 +1,27 @@
 # R/dna_codons_to_aa.R
 #Lucille coding :p
+dna_codons_to_aa <- function(codons){
+  #creates a "translation" table from the codon to the given amino acid
+  std_code_table <- c("TTT" = "F", "TCT" = "S", "TAT" = "Y", "TGT" = "C",
+                      "TTC" = "F", "TCC" = "S", "TAC" = "Y", "TGC" = "C",
+                      "TTA" = "L", "TCA" = "S", "TAA" = "*", "TGA" = "*",
+                      "TTG" = "L", "TCG" = "S", "TAG" = "*", "TGG" = "W",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTC" = "L", "CCC" = "P", "CAC" = "H", "CGC" = "R",
+                      "CTA" = "L", "CCA" = "P", "CAA" = "Q", "CGA" = "R",
+                      "CTG" = "L", "CCG" = "P", "CAG" = "Q", "CGG" = "R",
+                      "ATT" = "I", "ACT" = "T", "AAT" = "N", "AGT" = "S",
+                      "ATC" = "I", "ACC" = "T", "AAC" = "N", "AGC" = "S",
+                      "ATA" = "I", "ACA" = "T", "AAA" = "K", "AGA" = "R",
+                      "ATG" = "M", "ACG" = "T", "AAG" = "K", "AGG" = "R",
+                      "GTT" = "V", "GCT" = "A", "GAT" = "D", "GGT" = "G",
+                      "GTC" = "V", "GCC" = "A", "GAC" = "D", "GGC" = "G",
+                      "GTA" = "V", "GCA" = "A", "GAA" = "E", "GGA" = "G",
+                      "GTG" = "V", "GCG" = "A", "GAG" = "E", "GGG" = "G") 
+ 
+  # Paste0 concatenates the charecters into a string combined by no space (collapse = "")
+  aa <- paste0(std_code_table[codons], collapse = "")
+  
+  #returns the string of amino acids
+  return(aa)
+}
