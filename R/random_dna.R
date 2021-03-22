be3c3f808f69557c15db0a0d998140e6475126dd
signemarcussen <engis-10@hotmail.com>
2021-03-22 09:03:24 +0000

    add random_dna function

---
+++ R/random_dna.R
@@ -0,0 +1,8 @@
+random_dna <- function(l){
+  nucleotides <- sample(c("A", "T", "G", "C"), 
+                        size = l, 
+                        replace = TRUE)
+  dna = paste0(nucleotides, 
+               collapse = "")
+  return(dna)
+}
